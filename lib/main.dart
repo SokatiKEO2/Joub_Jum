7ec69f487dbb552bb4b65ce0ce57524acd943a71
Pisethysara-Vong <pisethysaravong@gmail.com>
2024-11-17 12:43:23 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:joub_jum/pages/map_page.dart';
-import 'package:joub_jum/pages/menu_bar_pages/account.dart';
-import 'package:joub_jum/pages/menu_bar_pages/friend.dart';
-import 'package:joub_jum/pages/menu_bar_pages/invitation.dart';
-import 'package:joub_jum/pages/menu_bar_pages/joub_jum.dart';
-import 'package:joub_jum/pages/menu_bar_pages/recommendation.dart';
+
 
 void main() => runApp(const MyApp());
 
